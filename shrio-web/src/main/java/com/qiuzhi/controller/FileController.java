cfa43d90469010e17dcbcec1c3605f06d76f585e
midnightwalking <781092958@qq.com>
2018-11-18 23:09:33 +0800

    新增jackjson包，登录方式修改，新增未登录无法访问某个目录的文件，

--- shrio-web/src/main/java/com/qiuzhi/controller/FileController.java
+++ shrio-web/src/main/java/com/qiuzhi/controller/FileController.java
@@ -4,13 +4,18 @@ import com.qiuzhi.dao.mapper.ImginfoMapper;
 import com.vo.AdPictureURL;
 import com.vo.Imginfo;
 import com.vo.ReturnObject;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import javax.servlet.ServletRequest;
 import javax.servlet.http.HttpServletRequest;
 import java.io.File;
 import java.io.IOException;
@@ -22,32 +27,37 @@ import java.util.*;
  * @description
  */
 @Controller
+@RequestMapping("/photo")
 public class FileController {
+
+    private final static Log logger = LogFactory.getLog(FileController.class);
 
     @Autowired
     private ImginfoMapper imginfoMapper;
 
     @RequestMapping(value = "/wangEditorUpload")
-    public AdPictureURL upload(@RequestParam List<MultipartFile> list) {
+    @CrossOrigin
+    @ResponseBody
+    public AdPictureURL upload(@RequestParam("photo") List<MultipartFile> list, ServletRequest request) {
         Integer error = 0;
         List<String> urls = new ArrayList<>();
         AdPictureURL returnAd = new AdPictureURL();
         Map<String, Object> map = new HashMap<>();
         List<String> adlist = new ArrayList<>();
+        logger.info("有"+list.size()+"张图片上传");
         if (list.size() == 0) {
             error = 1;
         }
-        Imginfo img = new Imginfo();
-
         for (MultipartFile file : list) {
-            String realName = "";
+            String realName = file.getOriginalFilename();
             if (file != null) {
-                String fileName = UUID.randomUUID().toString();
-                String path = "" + fileName;
-                File newFile = new File("" + fileName);
+                String fileName = UUID.randomUUID().toString()+realName;
+                String path = request.getServletContext().getRealPath("/") + "img/" + fileName;
+                logger.info("文件路径为：" + path);
+                File newFile = new File(path);
                 try {
                     file.transferTo(newFile);
-                    String databaseUrl = "http://192.168.1.58:8081/study/picture/" + fileName;
+                    String databaseUrl = request.getServletContext().getContextPath() + "/img/" + fileName;
                     urls.add(databaseUrl);
                 } catch (IOException e) {
                     error = 1;
