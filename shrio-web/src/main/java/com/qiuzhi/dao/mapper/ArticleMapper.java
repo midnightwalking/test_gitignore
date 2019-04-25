9937444b7eb845e033150a86855f83a1628be284
邱智 <qiuzhi2@worldunion.com.cn>
2019-04-25 17:34:07 +0800

    Revert "Revert "提交""
    This reverts commit 6e29180b

--- shrio-web/src/main/java/com/qiuzhi/dao/mapper/ArticleMapper.java
+++ shrio-web/src/main/java/com/qiuzhi/dao/mapper/ArticleMapper.java
@@ -4,9 +4,12 @@ import com.vo.Article;
 import com.vo.ArticleExample;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 /**
  * ArticleMapper继承基类
  */
 @Repository
 public interface ArticleMapper extends MyBatisBaseDao<Article, String, ArticleExample> {
+	List<Article> getNotZsetList();
 }
--- shrio-web/src/main/java/com/qiuzhi/utils/JedisUtil.java
+++ shrio-web/src/main/java/com/qiuzhi/utils/JedisUtil.java
@@ -1,11 +1,14 @@
 package com.qiuzhi.utils;
 
+import com.qiuzhi.dao.mapper.ArticleMapper;
+import com.vo.Article;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 
 import javax.annotation.Resource;
+import java.util.List;
 import java.util.Set;
 
 /**
@@ -19,6 +22,9 @@ public class JedisUtil {
 
     @Resource(name = "jedisPool")
     private JedisPool jedisPool;
+
+    @Autowired
+    private ArticleMapper articleMapper;
 
     private Jedis getResource(){
         return jedisPool.getResource();
@@ -65,6 +71,58 @@ public class JedisUtil {
         Jedis jedis = getResource();
         try{
             return jedis.keys((prefix + "*").getBytes());
+        }finally {
+            jedis.close();
+        }
+    }
+
+
+    public void zSetArticles(List<Article> articles){
+        Jedis jedis = getResource();
+        try {
+            for (Article article : articles) {
+                String id = article.getId();
+                String title = article.getTitle();
+                jedis.zadd("hotArticles", 0, id + "@" + title);
+                article.setIs_zset("1");
+                articleMapper.updateByPrimaryKey(article);
+            }
+        }finally {
+            jedis.close();
+        }
+    }
+
+    /**
+     * @Author qiuzhi
+     * @Description 获取排行榜
+     * @Date 18:05 2019/1/29
+     * @Param []
+     * @return java.util.Set<java.lang.String>
+     **/
+
+    public Set<String> getHotArticle(){
+        Jedis jedis = getResource();
+        try {
+            Set<String> hotArticles = jedis.zrevrange("hotArticles", 0, -1);
+            return hotArticles;
+        }finally {
+            jedis.close();
+        }
+    }
+
+    /**
+     * @Author qiuzhi
+     * @Description 更新分数
+     * @Date 18:05 2019/1/29
+     * @Param [key]
+     * @return void
+     **/
+
+    public void addScoreByKey(String key){
+        Jedis jedis = getResource();
+        try {
+           double addResult = jedis.zincrby("hotArticles",1,key);
+           System.out.println("更新结果：" + addResult);
         }finally {
             jedis.close();
         }
